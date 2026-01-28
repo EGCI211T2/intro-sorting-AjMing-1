7cfd6585a7e73fc8fef606b29f42f4859b482161
AjMing <62439174+AjMing@users.noreply.github.com>
2026-01-28 08:53:13 +0000

    add selection sort

--- sorting.h
+++ sorting.h
@@ -19,16 +19,37 @@ void display(int a[],int n){
     cout<<endl;
 }
 
-void sort(int *a,int N){
+void bubblesort(int *a,int N){
    int i,j;//
-  for(j=0;j<N-1;j++){
-    for(i=0;i<N-1-j;i++){
+   int sorted;
+  for(j=N-1;j>0;j--){
+    sorted=0;
+    for(i=0;i<j;i++){
                 /*For ascending order*/
-                if(a[i]> a[i+1] ) //0 1 -> 1 2 -> 2 3 -> 3 4  --> n-2 n-1
+                if(a[i]> a[i+1] ){ 
                     swap(a[i],a[i+1]);
+                    sorted=1;
+                }
     }
     display(a,N);
+    if(sorted==0) break;
    }
 
 
+}
+
+void selectionsort(int *a,int N){
+    int i,mi,min,j;
+
+   for(j=0;j<N-1;j++){
+        min=a[j];
+            mi=j;
+        for(i=j+1;i<N;i++){
+            if(min >a[i]){
+                min = a[i];
+                mi= i;
+            }
+        }
+        swap(a[j],a[mi]);// a[0] <->a[7]
+   }
 }
